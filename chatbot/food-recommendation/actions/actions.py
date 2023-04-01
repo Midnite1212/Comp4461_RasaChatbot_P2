4be7db5818c5fd76432a4e159e0ef5619e64621a
Richie <ricia1212@gmail.com>
2023-04-01 15:06:44 +0800

    Basic functionalities for food recommendation works, need to update data content

--- chatbot/food-recommendation/actions/actions.py
+++ chatbot/food-recommendation/actions/actions.py
@@ -5,39 +5,40 @@ from rasa_sdk.events import EventType
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
-ALLOWED_PIZZA_SIZES = ["small", "medium", "large", "extra-large", "extra large", "s", "m", "l", "xl"]
-ALLOWED_PIZZA_TYPES = ["mozzarella", "fungi", "veggie", "pepperoni", "hawaii"]
+ALLOWED_FOOD_CATEGORIES = ["cuisine", "restriction", "calories", "diet", "healthy"]
+ALLOWED_FOOD_TYPES = ["indian", "japanese", "korean", "western", "vegan", "glutten-free", "lose-weight", "body-building", "balanced"]
+FOOD_IMAGES= ["https://imgur.com/xnRuJVr", "https://i.imgur.com/nGF1K8f.jpg"]
 
-class ValidateSimplePizzaForm(FormValidationAction):
+class ValidateSimpleFoodForm(FormValidationAction):
     def name(self) -> Text:
-        return "validate_simple_pizza_form"
+        return "validate_food_recommendation_form"
 
-    def validate_pizza_size(
+    def validate_food_category(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        """Validate `pizza_size` value."""
+        """Validate `food_category` value."""
 
-        if slot_value.lower() not in ALLOWED_PIZZA_SIZES:
-            dispatcher.utter_message(text=f"We only accept pizza sizes: s/m/l/xl.")
-            return {"pizza_size": None}
-        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
-        return {"pizza_size": slot_value}
+        if slot_value.lower() not in ALLOWED_FOOD_CATEGORIES:
+            dispatcher.utter_message(text=f"We only accept food categories: cuisine / restriction diet / calories diet.")
+            return {"food_category": None}
+        dispatcher.utter_message(text=f"OK! You want to have {slot_value} food.")
+        return {"food_category": slot_value}
 
-    def validate_pizza_type(
+    def validate_food_type(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        """Validate `pizza_type` value."""
+        """Validate `food_type` value."""
 
-        if slot_value not in ALLOWED_PIZZA_TYPES:
-            dispatcher.utter_message(text=f"I don't recognize that pizza. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}.")
-            return {"pizza_type": None}
-        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} pizza.")
-        return {"pizza_type": slot_value}
+        if slot_value not in ALLOWED_FOOD_TYPES:
+            dispatcher.utter_message(text=f"I don't recognize that food type. We serve {'/'.join(ALLOWED_FOOD_TYPES)}.")
+            return {"food_type": None}
+        dispatcher.utter_message(text=f"OK! You want to have {slot_value} food.")
+        return {"food_type": slot_value, "food_img": FOOD_IMAGES[0]}
